7b801fb42f450d5bc11a9b664495f5c607d19671
Tickether <tickether@gmail.com>
2022-11-20 16:55:26 +0000

    reset paas
    some updates..testing..

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -44,14 +44,14 @@ const Login = () => {
       <div className="lContainer">
         <input 
           type="text" 
-          placeholder='email' 
+          placeholder='Email' 
           id='email' 
           onChange={handleChange} 
           className="lInput" 
         />
         <input 
           type="password" 
-          placeholder='password' 
+          placeholder='Password' 
           id='password' 
           onChange={handleChange} 
           className="lInput" 
--- admin/src/pages/resetPassword/ResetPassword.jsx
+++ admin/src/pages/resetPassword/ResetPassword.jsx
@@ -1,16 +1,20 @@
 import './resetPassword.scss';
 import { useState } from 'react'
 import axios from 'axios';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const ResetPassword = () => {
   const [credentials, setCredentials] = useState({
     password: undefined,
   });
+  const [validUrl, setValidUrl] = useState(false)
 
   const param = useParams()
 
   const [error, setError] = useState()
+
+  
     
   const navigate = useNavigate();
 
@@ -18,10 +22,27 @@ const ResetPassword = () => {
     setCredentials((prev)=>({...prev, [e.target.id]:e.target.value}))
   };
 
+  
+
+  useEffect(() => {
+      const verifyEmailUrl = async () => {
+          try {
+              const res = axios.get(`https://api.tickether.io/api/bookers/${param.id}/verify/${param.token}`)
+              console.log(res);
+              setValidUrl(true)
+          } catch (err) {
+              console.log(err)
+              setValidUrl(false)
+          }
+      }
+      verifyEmailUrl();
+  },[param])
+
+  
   const handleCLick = async e => {
     e.preventDefault() 
     try{
-      const res = await axios.get(`https://api.tickether.io/api/bookers/${param.id}/forgotpassword/${param.token}`, credentials);
+      const res = await axios.put(`https://api.tickether.io/api/bookers/${param.id}`, credentials);
       console.log(res)
       navigate('/login')
     } catch(err) {
@@ -33,16 +54,26 @@ const ResetPassword = () => {
 
   return(
     <div className="resetPassword"> 
-      <div className="rpContainer">
-        <input 
-          type="password" 
-          placeholder='password' 
-          id='password' 
-          onChange={handleChange} 
-          className="rpInput" 
-        />
-        <button onClick={handleCLick} className="rpButton">Reset</button>
-        {error && <span>{error}</span>}
+      <div className="rpContainer"> 
+        {validUrl ? (
+                <div>
+                    <input 
+                      type="password" 
+                      placeholder='password' 
+                      id='password' 
+                      onChange={handleChange} 
+                      className="rpInput" 
+                    />
+                    <button onClick={handleCLick} className="rpButton">Reset</button>
+                    <p>Already reset account? <Link to='/login'>Login</Link></p>
+                    {error && <span>{error}</span>}
+                </div>
+            ) : (
+                <div>
+                    <span>404 Not Found</span>
+                </div>
+            )
+        }
       </div>
     </div>
   )
